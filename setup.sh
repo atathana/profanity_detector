1bbe0cca54b39bba2ed1ae88cbcfa810a04f5c56
Adelina Ionescu <ionescu.adelina1012@gmail.com>
2022-03-01 20:25:54 +0100

    updating the theme

--- setup.sh
+++ setup.sh
@@ -6,8 +6,10 @@ email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+[theme]\n\
+base=\"dark\"\n\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" >> ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
