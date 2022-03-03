a1f3275af3dd0c7935eb19dd64ba422c1b8f75ff
Tomer Erenshtein <tomerenshtein@gmail.com>
2022-03-03 22:30:35 +0100

    linited locations to 35 - limit of nominatim

--- profanity_detector/feature_data_enrichment.py
+++ profanity_detector/feature_data_enrichment.py
@@ -31,9 +31,11 @@ def get_coordinates(location):
     '''
     get coordinates, if immposible - extract city, region, country and get approximate coordinates
     '''
-    
+    #request coordinates
     loc = Nominatim(user_agent="profanity_detector")
     getLoc = loc.geocode(location)
+    
+    # check if coordinates found, if not normalize location string and get aprox location
     if getLoc is None:
         
         #instatiate geofrapy
@@ -64,6 +66,13 @@ def get_coordinates(location):
         #get coordinates for alt_location
         getLoc = loc.geocode(alt_location)
 
+    # if still coordinates not found - drop city
+    if getLoc is None:
+        alt_list = alt_location.split(',')
+        alt_list.remove(alt_list[0])
+        alt_location = ', '.join(alt_list)
+        getLoc = loc.geocode(alt_location)    
+
     latitude = getLoc.latitude
     longitude = getLoc.longitude
     
@@ -73,7 +82,14 @@ def get_coordinates(location):
 
 
 def enrich_locations(locations_df):
+    #limit to 35 locations - linit of nominatim
+    location_df = locations_df.iloc[:35,:]
+
+    #split scene names  string from geolocation strings
     locations_df['movie_location'] = locations_df.apply(lambda x: get_movie_location(x['locations']), axis=1 )
     locations_df['set_location'] = locations_df.apply(lambda x: get_set_location(x['locations']), axis=1 )
+    
+    #get coordinates
     locations_df[['latitude','longitude']] = locations_df.apply(lambda x: get_coordinates(x['set_location']) , axis=1, result_type='expand')
+    
     return locations_df
