2571e75dd7dcc84a191e72e56ab730f6d3a50af5
Nasia Athanasiadou <nasioulita@gmail.com>
2022-03-08 22:09:21 +0100

    New geolocation tab

--- app_new.py
+++ app_new.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, sentiment, spotify  # import your app modules here
+from apps import home, sentiment, spotify,locations  # import your app modules here
 
 
 app = MultiApp()
@@ -9,6 +9,9 @@ app = MultiApp()
 app.add_app("Home", home.app)
 app.add_app("Sentiment", sentiment.app)
 app.add_app("Spotify", spotify.app)
+app.add_app("Locations", locations.app)
+
+
 # The main app
 
 app.run()
